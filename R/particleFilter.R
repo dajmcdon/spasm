6afbc432e15ad0f78e0056fa9004fcba6ad4fbc7
mtmcbride <michmcbr@indiana.edu>
2017-07-01 17:18:39 -0400

    added a resampling criterion

--- R/particleFilter.R
+++ R/particleFilter.R
@@ -11,14 +11,18 @@ particleFilter <- function(Yin, stateTransition,
         }
         weights = f(Y[t,], particles)
         weights = weights/sum(weights)
-        indices = sample(1:nrow(particles), prob = weights, replace = TRUE)
+        if(sum(weights^2) > t/2){
+            indices = sample(1:nrow(particles), prob = weights, replace = TRUE)
+        }
+        else{
+            indices = 1:nrow(particles)
+        }
         output[t,,] = particles[indices,]
         particles = as.matrix(stateTransition(as.matrix(particles[indices,])))
     }
     output
 }
 
-dat =  generateSPASM(1000, 3, 2)
 stateTransition <- function(x){
     (0.9 * x) + (0.1 * rnorm(length(x)))
 }
@@ -45,6 +49,7 @@ gp <- function(n){
 
 
 #' @export
-filterTest <- function(){
-    particleFilter(dat$y, stateTransition = stateT, f = ygx, generateParticles = gp, verbose = TRUE)
+filterTest <- function(TT, np){
+    dat =  generateSPASM(TT, 3, 2)
+    particleFilter(dat$y, stateTransition = stateT, f = ygx, generateParticles = gp, verbose = TRUE, nParticles = np)
 }
